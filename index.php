2a7851a9114d7ecb225c6154243bba260f7ad401
MelissaMejia <melissa.gms93@gmail.com>
2017-01-10 15:14:27 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,12 +1,20 @@
 <?php
- echo "ultimo";
- if($_SERVER['REQUEST_METHOD']=='POST'){
+ echo "ultima";
+if($_SERVER['REQUEST_METHOD']=='POST'){
+
  $image = $_POST['image'];
  $image_name = $_POST['image_name'];
  $decodificarimagen=base64_decode("$image");
- if(file_put_contents("gs://imagenes_ubigiis/hello.txt", "hola")){ 	
- echo "Imagen subida satisfactoriamente".$image_name;
+
+$options = ['gs' => ['acl' => 'public-read']];
+$context = stream_context_create($options);
+$fileName = "gs://imagenes_ubigiis/".$image_name.".JPG";
+if(file_put_contents($fileName, $decodificarimagen, 0, $context)){
+echo "Imagen subida satisfactoriamente".$image_name;
  }else{
  echo "Error al subir la imagen";
- }}
+ }
+}
+
+
   ?>
