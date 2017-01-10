272c0ba670ccc96fb6bc00c451472d539023e530
MelissaMejia <melissa.gms93@gmail.com>
2017-01-10 15:02:45 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,14 +1,12 @@
 <?php
  echo "hola";
  if($_SERVER['REQUEST_METHOD']=='POST'){
- 
  $image = $_POST['image'];
  $image_name = $_POST['image_name'];
  $decodificarimagen=base64_decode("$image");
- if(file_put_contents("imagenes/".$image_name.".JPG",$decodificarimagen)){ 	
- echo "Imagen subida satisfactoriamente";
+ if(file_put_contents("gs://ubigiis_imagenes/hello.txt", "hola")){ 	
+ echo "Imagen subida satisfactoriamente".$image_name;
  }else{
  echo "Error al subir la imagen";
  }}
-
   ?>
