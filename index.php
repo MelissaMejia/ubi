ef0bb76dcbc8d545cc064b21da09dfce47e6f080
MelissaMejia <melissa.gms93@gmail.com>
2017-01-10 15:05:34 -0500

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
  $image = $_POST['image'];
  $image_name = $_POST['image_name'];
  $decodificarimagen=base64_decode("$image");
- if(file_put_contents("gs://ubigiis_imagenes/hello.txt", "hola")){ 	
+ if(file_put_contents("gs://imagenes_ubigiis/hello.txt", "hola")){ 	
  echo "Imagen subida satisfactoriamente".$image_name;
  }else{
  echo "Error al subir la imagen";
